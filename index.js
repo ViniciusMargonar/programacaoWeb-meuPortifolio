d703a8e6e339c874d622d4c017445f9bac93e4b6
Vinícius Margonar <vinimargonar@hotmail.com.br>
2024-07-14 20:43:46 -0300

    Adição do uso da API

--- index.js
+++ index.js
@@ -48,3 +48,31 @@ function copiarDados() {
     const todosOsDados = `${dados1}\n${dados2}`;
     navigator.clipboard.writeText(todosOsDados);
 }
+
+
+
+
+
+
+const wrapper =document.querySelector(".wrapper"),
+qrInput = wrapper.querySelector(".form input"),
+generateBtn =wrapper.querySelector(".form button"),
+qrImg = wrapper.querySelector(".qr-code img");
+
+generateBtn.addEventListener("click", () => {
+    let qrValue = qrInput.value;
+    if(!qrValue) return;
+    generateBtn.innerText = "Gerando um Qr Code..."
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+    qrImg.addEventListener("load", () => {
+        generateBtn.innerText = "Gerar Qr Code"
+        wrapper.classList.add("active");
+    });
+   
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value){
+        wrapper.classList.remove("active");
+    }
+});
